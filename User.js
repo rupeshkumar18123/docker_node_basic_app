5d60e86b4ba473db46e94f984facc5d283433255
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-24 12:41:08 +0530

    create a user module

---
+++ User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+ 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+       
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
